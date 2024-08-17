c8fbfb6ea3da8ca16bc3619231b052652a4204f3
Tewaa <123607334+TewaaCao@users.noreply.github.com>
2024-08-16 21:55:10 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
-importScripts(__uv$config.sw || '/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js')
+importScripts(__uv$config.sw || '/uv/uv.sw.js')
 
 const sw = new UVServiceWorker();
 
